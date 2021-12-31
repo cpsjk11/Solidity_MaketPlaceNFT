cfdb5ee54ab8af42ab53a408d0f4c1f72820ab86
cpsjk11 <87220691+cpsjk11@users.noreply.github.com>
2021-12-31 22:59:45 +0900

    정의한 스마트컨트랙 연결부분

--- migrations/2_MaketplaceNFT.js
+++ migrations/2_MaketplaceNFT.js
@@ -1,5 +1,6 @@
 const Maket = artifacts.require("Maket");
-
+const Auctions = artifacts.require("Auctions");
 module.exports = async function(deployer){
     deployer.deploy(Maket,"AvarCat","Acat");
+    deployer.deploy(Auctions);
 };
